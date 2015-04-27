5bfde160320e04a49da0a99597a7dbfedc9bc22e
AlcatrazGR <alcatrazgr0@gmail.com>
2015-04-27 20:10:25 +0300

    Javascript calling code methods

---
+++ Tutorials/Javascript Call Types/externalFile.js
@@ -0,0 +1,11 @@
+function SayHello(){
+	alert('Hello Visitor, feel free to browse our page!');
+}
+
+function ChangeColorToRed(){
+	document.getElementById('message').style.color = "red";
+}
+
+function ChangeColorToGreen(){
+	document.getElementById('message').style.color = "green";
+}
