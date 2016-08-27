a3dcc92c01c815dbae9b14bdede2ba1bf3ed17e0
AlcatrazGR <alcatrazgr0@gmail.com>
2016-08-27 19:03:00 +0300

    real progress bar tutorial with real application

---
+++ Tutorials/Bootstrap/10. Progress Bars/Real Progress Bar Example/server.php
@@ -0,0 +1,8 @@
+<?php
+	echo "Legnth : ".$_SERVER['CONTENT_LENGTH'];
+	
+	$tmp_file = $_FILES['fileField']['tmp_name'];
+	$fileName = $_FILES['fileField']['name'];
+
+	move_uploaded_file($tmp_file, 'Uploads/'.$fileName);
+?>
