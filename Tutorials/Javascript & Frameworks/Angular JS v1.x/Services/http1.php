687bd5942443284e122be0d7db9f02f6d14b7b48
AlcatrazGR <alcatrazgr0@gmail.com>
2018-04-19 19:38:04 +0300

    Angular HTTP service Part 1

--- Tutorials/Javascript & Frameworks/Angular JS v1.x/Services/http1.php
+++ Tutorials/Javascript & Frameworks/Angular JS v1.x/Services/http1.php
@@ -30,6 +30,20 @@
                 margin-top: 10px;
                 border-radius: 5px;
             }
+            #tableHeader {
+                text-align: center;
+                background: #525050;
+                font-size: 17px;
+            }
+            #tableHeader > div {
+                border: 1px solid #757373;
+                padding: 5px;
+            }
+            .userDataRow {
+                background: #5d5d5d;
+                border-bottom: 1px solid #717171;
+                padding: 5px 0px 5px 0px;
+            }
         </style>
     </head>
     <body>
@@ -61,6 +75,25 @@
         </div>
 
         <div id="httpApp" ng-app="httpApp" ng-controller="httpAppController" class="container">
+            <div class="example-code-snippet-area"> <code>
+                $scope.searchRes = $location.search();
+            </code> </div>
+            <div class="example-result-area">
+                <div class='{{httpReqStatusClass}}'> {{ httpMessage }} </div>
+                <div class="row" id="tableHeader">
+                    <div class="col-md-1"> A/A </div>
+                    <div class="col-md-4"> User Name </div>
+                    <div class="col-md-5"> Email </div>
+                    <div class="col-md-2"> Balance </div>
+                </div>
+                <div ng-repeat="user in users" class="row userDataRow">
+                    <div class="col-md-1"> {{user.index}} </div>
+                    <div class="col-md-4"> {{user.fullname}} </div>
+                    <div class="col-md-5"> {{user.email}} </div>
+                    <div class="col-md-2"> {{user.balance | currency : 2 }} </div>
+                </div>
+            </div>
+
 
         </div> <!-- /httpApp -->
 
@@ -69,6 +102,28 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script type="text/javascript">
+
+            var app = angular.module("httpApp", []);
+            app.controller("httpAppController", function( $scope, $http ) {
+
+                $http.post("./userdata.json")
+                .then(function( response ) {
+                    var statusCode = response.status;
+
+                    if( statusCode == 200 ) {
+                        $scope.httpReqStatusClass = "alert alert-success";
+                        $scope.httpMessage = "The request has been made and retrived response successfully!";
+                        var users = response.data.items;
+                        $scope.users = users;
+                    } else {
+                        $scope.httpReqStatusClass = "alert alert-danger";
+                        $scope.httpMessage = "Error on request: " + response.statusText;
+                    }
+
+
+                }); // end of http post
+
+            });
 
         </script>
     </body>
