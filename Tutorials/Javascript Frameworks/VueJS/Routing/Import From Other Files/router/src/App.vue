3b3f76063db8113558bc20f983d506a552fa18a6
AlcatrazGR <alcatrazgr0@gmail.com>
2019-07-27 00:16:26 +0300

    start routing

---
+++ Tutorials/Javascript Frameworks/VueJS/Routing/Import From Other Files/router/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="app">
+		<pagestructure></pagestructure>
+
+
+
+<!-- 		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+
+		<router-view/> -->
+	</div>
+</template>
+
+<style type="text/css" src="./css/style.css?v=1"></style>
+
+<script>
+	import pagestructure from "./components/Page.Structure.Component";
+	export default {
+		name: 'app',
+		data() {
+			return { }
+		},
+		components: {
+			pagestructure
+		}
+	}
+</script>
---
+++ Tutorials/Javascript Frameworks/VueJS/Routing/Import From Other Files/router/src/components/Page.Structure.Component.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<header>
+			<div class="header-logo"> <img src="../assets/header_logo.png" /> </div>
+			<div class="header-menu-binder" id="nav">
+				<router-link to="/">Home</router-link> |
+				<router-link to="/about">News</router-link>
+				<router-link to="/about">About</router-link>
+				<router-link to="/about">Contact</router-link>
+			</div>
+		</header>
+		<div class="main-content-binder">
+			<aside>
+				<div class="side-menu-sel"> Programming News </div>
+				<div class="side-menu-sel"> Scientific News </div>
+				<div class="side-menu-sel"> Weather News </div>
+				<div class="side-menu-sel"> Economy News </div>
+			</aside>
+			<main><router-view/></main>
+		</div>
+		<footer>
+			<div class="footer-content-binder">
+				<div class="footer-links">
+					<router-link to="/about">About</router-link>
+					<router-link to="/about">Contact</router-link>
+				</div>
+				<div class="footer-logo">
+					
+				</div>
+				<div class="footer-owner-rights"></div>
+			</div>
+		</footer>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'pagestructure'
+	}
+</script>
