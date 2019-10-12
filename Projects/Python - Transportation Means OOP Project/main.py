6b1e1e99cbdbd9a73cfa1a79e3f70dfc9e6e5311
AlcatrazGR <alcatrazgr0@gmail.com>
2019-10-12 16:41:35 +0300

    Python project refactor & database creation

--- Projects/Python - Transportation Means OOP Project/main.py
+++ Projects/Python - Transportation Means OOP Project/main.py
@@ -2,39 +2,74 @@ from monorail import Monorail
 
 def DisplayMenu():
     menuFlag = True
-
     while menuFlag:
         print("============ Transportation Means Application ============")
         print("Please select an option: ")
         print("\t 1. Display Registered Transportation Means")
-        print("\t 2. Register a New Transportation Vehicle")
-        print("\t 3. Delete a Registered Transportation Vehicle")
+        print("\t 2. Register Transportation Means")
+        print("\t 3. Register Transportation Means System")
+        print("\t 4. Register a New Transportation Vehicle")
+        print("\t 5. Delete a Registered Transportation Vehicle")
         print("==========================================================")
 
         selection = int(input())
-        if selection == 1 or selection == 2 or selection == 3:
+        if selection == 1 or selection == 2 or selection == 3 or selection == 4 or selection == 5:
             menuFlag = False
 
-    return selection
+    if selection == 1:
+        # asdadsd
+    elif selection == 2:
+        # asdsadsa
+    elif selection == 3:
+        # asd sa
+    elif selection == 4:
+        # asd asdad
+    elif selection == 5:
+        # asd asdad
+
+def RegisterTransportationMeans:
+
+
+
+
+
+
+
+
+
+
+def TransportationMeansSelector():
+    handler = open("./Database/Transportation.Means.Database.json")
 
 def DisplayRegisteredMeans():
-    print("============ Transportation Means Categories ============")
-    print("Select the means category: ")
-    print("\t 1. Rapid Transit")
-    print("\t 2. Rail Ways")
-    print("\t 3. Water Ways")
-    print("\t 4. Air Ways")
-    print("=========================================================")
+    menuFlag = True
+    while menuFlag:
+        print("============ Transportation Means Categories ============")
+        print("Select the means category: ")
+        print("\t 1. Rapid Transit")
+        print("\t 2. Rail Ways")
+        print("\t 3. Water Ways")
+        print("\t 4. Air Ways")
+        print("=========================================================")
 
+        selection = int(input())
+        if selection == 1 or selection == 2 or selection == 3 or selection == 4:
+            menuFlag = False
 
-
-def MenuSelectionController(selection):
     if selection == 1:
+        # TODO: Rapid Transit
+    elif selection == 2:
+        # TODO: Rail Ways
+    elif selection == 3:
+        # TODO: Water Ways
+    elif selection == 4:
+        # TODO: Air Ways
 
-    else if selection == 2:
 
-    else if selection == 3:
 
+
+
+def RegisterNewTransportationMeans():
 
 
 DisplayMenu()
