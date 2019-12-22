3f91ba70a32ce4f4ef3a80e108b36a86b6f2ee11
AlcatrazGR <alcatrazgr0@gmail.com>
2019-12-22 18:19:54 +0200

    adding builder params and necessary arguments

--- Projects/Nodejs - Notes App/app.js
+++ Projects/Nodejs - Notes App/app.js
@@ -1,15 +1,52 @@
+const yargs = require("yargs");
 const validator = require("validator");
 const chalk = require("chalk");
+const fs = require("fs");
 
-console.log(process.argv);
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	builder: {
+		title: {
+			describe: 'Note`s title',
+			demandOption: true, 	// Attribute that makes the title to be necessary.
+			type: 'string'			// The data type that the parameter must be.
+		},
+		body: {
+			describe: 'Note`s content',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		console.log("Title: " + argv.title);
+		console.log("Body: " + argv.body);
+	}
+});
 
-const command = process.argv[2];
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function () {
+		console.log("removing note");
+	}
+});
 
-if(command === 'add') {
-	console.log("adding note");
-}
-else if(command === 'remove') {
-	console.log("removing note");
-} else {
-	console.log("wrong command");
-}
+yargs.command({
+	command: 'read',
+	describe: 'Reading notes',
+	handler: function () {
+		console.log("reading notes");
+	}
+});
+
+yargs.command({
+	command: 'list',
+	describe: 'Listing notes',
+	handler: function () {
+		console.log("listing notes");
+	}
+});
+
+// This is necessary to let yargs know that it must parse the argumets
+yargs.parse(); 
