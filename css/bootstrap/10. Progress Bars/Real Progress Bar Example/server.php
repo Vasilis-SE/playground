b9d2b7ca539971688b2311a461b1ff6c908187e0
Vasilis CodeX <vasilis.tris@gmail.com>
2022-06-28 00:17:04 +0300

    removed css folder

--- css/bootstrap/10. Progress Bars/Real Progress Bar Example/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo "Legnth : ".$_SERVER['CONTENT_LENGTH'];
-	
-	$tmp_file = $_FILES['fileField']['tmp_name'];
-	$fileName = $_FILES['fileField']['name'];
-
-	move_uploaded_file($tmp_file, 'Uploads/'.$fileName);
-?>
