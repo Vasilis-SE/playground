f9f889e92ec07f05d124719651a217e4d073333b
Vasilis CodeX <vasilis.tris@gmail.com>
2025-04-26 16:36:59 +0300

    restructure playground folder

---
+++ java/spring_kafka/src/main/java/com/spring/kafka/SpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+
+}
---
+++ java/spring_kafka/src/test/java/com/spring/kafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
